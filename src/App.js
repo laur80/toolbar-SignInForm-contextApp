ea24d3377020122d1017813f4032ff8b33027aef
laur80 <laur80raul@gmail.com>
2020-10-13 23:11:50 +0300

    add app with color context only

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./Navbar"
+import Form from "./Form"
+import PageContent from "./PageContent"
+
+import { ThemeProvider, ThemeContext } from "./contexts/ThemeContext";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <ThemeProvider>
+    <PageContent>
+    <Navbar />
+    <Form />
+    </PageContent>
+    </ThemeProvider>
+    </>
+
   );
 }
 
 export default App;
+
---
+++ src/hooks/useToggleState.js
@@ -0,0 +1,10 @@
+import {useState} from 'react'
+
+export default function useToggle(initVal=false) {
+
+    const [state,setState]=useState(initVal);
+    const toggle=()=>{
+        setState(!state);}
+
+    return [state,toggle];
+}
